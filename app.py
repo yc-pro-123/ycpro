6c03aaeb37e5c234e8b3dcb515bb21cf98aedbeb
Master of None <131793324+yc-pro-123@users.noreply.github.com>
2024-09-12 13:49:19 +0530

    Update app.py hm j

--- app.py
+++ app.py
@@ -109,9 +109,9 @@ def instavideosave():
     ur=ct_bytes.hex() 
     #url ="https://twitter.com/TweetTemplates1/status/1809197143099670530" #params={"url":"https://www.instagram.com/reel/C_ktpDXSW9l/?utm_source=ig_web_button_share_sheet"}
     downurl="https://dl1.instavideosave.com/?url="
-    sc="""<html lang='en-US'><head><title>Hellll</title>
+    sc="""<!DOCTYPE html><html lang='en-US'><head><title>Hellll</title>
     <meta charset='utf-8'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
     <h1>Yours Url:"""
